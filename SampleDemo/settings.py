f495fd9d6a7caa0d488836dfdfd717c1b113c2aa
K S, Nethravathi <nethravathi.k.s@accenture.com>
2022-09-15 13:26:40 +0530

    added files

--- SampleDemo/settings.py
+++ SampleDemo/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'hqp1fv1zq6hhkq)=$f&wtl9vsbuw78^3h4lveg4wmn@w9xqk7!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.getenv('DEBUG'))
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -78,15 +78,15 @@ WSGI_APPLICATION = 'SampleDemo.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USERNAME'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'sample',
+            'USER': 'postgres',
+            'PASSWORD': 'Cloud123',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
